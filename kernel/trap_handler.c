d00993e7c82ca54f1284ca508753c4c0b613002f
cryphon <yvanroes@gmail.com>
2026-04-28 23:24:24 +0000

    trap: update trap handler to change behavior on mcause type

--- kernel/trap_handler.c
+++ kernel/trap_handler.c
@@ -17,15 +17,46 @@
 
 /* --- Private Function Prototypes ----------------------------------------- */
 
+void uart_print_hex(uint32_t val) {
+    uart_puts("0x");
+    for (int i = 7; i >= 0; i--) {
+        uint8_t nibble = (val >> (i * 4)) & 0xF;
+        char c = nibble < 10 ? '0' + nibble : 'a' + (nibble - 10);
+        uart_putc(c);
+    }
+}
+
 /* --- Public Functions ---------------------------------------------------- */
 
 void trap_handler(void)
 {
-    uart_puts("Trap fired!\n");
+    uint32_t cause = read_csr(mcause);
+    uint32_t epc   = read_csr(mepc);
+    
+    uart_puts("Trap! cause=");
+    uart_print_hex(cause);
+    uart_puts(" mepc=");
+    uart_print_hex(epc);
+    uart_puts("\n");
 
-    // Advance past the caller
-    uint32_t epc = read_csr(mepc);
-    write_csr(mepc, epc + 4);
+    switch(cause)
+    {
+        case 8:         // Environment call from U-Mode
+        case 9:         // Environment call from S-Mode
+        case 11:        // Environment call from M-Mode
+            uart_puts("Trap: ecall\n");
+            write_csr(mepc,  read_csr(mepc) + 4); // only advance for ecall
+            break;
+        case 2:
+            uart_puts("Trap: illegal instruction\n");
+            break;
+        case 5:
+            uart_puts("Trap: load access fault\n");
+            break;
+        default:
+            uart_puts("Trap: Unknown\n");
+            break;
+    }
 }
 
 /* --- Private Functions --------------------------------------------------- */
