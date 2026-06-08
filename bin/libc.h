a45a56db0273d4de4735060ff869dc8b28b48756
cryphon <yvanroes@gmail.com>
2026-06-08 23:28:58 +0000

    feat: add libc for userspace defs

---
+++ bin/libc.h
@@ -0,0 +1,21 @@
+/*
+ * libc.h
+ * Description of this file.
+ */
+
+#ifndef LIBC_H
+#define LIBC_H
+
+/* --- Includes ------------------------------------------------------------ */
+
+/* --- Macros / Constants -------------------------------------------------- */
+/* NEEDS TO MATCH TYPES IN syscall_nr.h */
+#define STDOUT 1
+#define SYS_WRITE 64
+#define SYS_EXIT 93
+
+/* --- Types / Structs ----------------------------------------------------- */
+
+/* --- Public Function Prototypes ------------------------------------------ */
+
+#endif /* LIBC_H */
