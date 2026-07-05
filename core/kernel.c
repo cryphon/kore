2c20c8eb1fd5429dd7487eb2ef188676d9761c98
cryphon <yvanroes@gmail.com>
2026-07-05 00:34:11 +0000

    feat: add second static process

--- core/kernel.c
+++ core/kernel.c
@@ -23,6 +23,8 @@ void kernel_main(void) {
     // TODO: no bounds checking yet on pid in proc_init
     proc_init(0, (void*)__process_stack_pool_start + PROCESS_STACK_SIZE);
     log_info("Hello from kernel\n"); 
+
+    proc_init(1, (void*)__process_stack_pool_start + (2 * PROCESS_STACK_SIZE));
     /* Jump to U-mode */
     switch_to_umode((uint32_t)umode_entry, tasks[0]);
     
