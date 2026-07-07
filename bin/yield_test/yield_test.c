4ad8267624f48bc602a09eb817cb24ccf1d9fab9
cryphon <yvanroes@gmail.com>
2026-07-07 18:42:32 +0000

    fixup! feat build(userspace): scaffold independent per-binary linking pipeline (WIP, entry wiring incomplete) [x.y]

--- bin/yield_test/yield_test.c
+++ bin/yield_test/yield_test.c
@@ -15,8 +15,7 @@
 /* --- Private Function Prototypes ----------------------------------------- */
 
 /* --- Public Functions ---------------------------------------------------- */
-// TODO: rename main_yield back to main after independent links
-int main_yield(void)
+int main(void)
 {
     int result = sys_write(STDOUT, "Hello from yield\n", 16);
     return 0;
